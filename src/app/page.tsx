dbee5685a7e060a7fd0cd83854f4e572babe1b64
Saud Zubair <m.saud840@gmail.com>
2024-10-26 00:31:50 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,140 @@
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className="bg-blue-900 h-16 text-2xl font-extrabold text-white p-8">
+        <h1 className="text-center mr-12 ">Tuition Free Education Program on Latest Technologies</h1>
+      </div>
+      <div className="bg-blue-900 text-1xl font-xs text-white text-right h-14 -mt-6">
+        <Link className="mr-4 " href=""> Home</Link>
+        <Link className="mr-4" href=""> Apply</Link>
+        <Link className="mr-4" href=""> Jobs</Link>
+        <Link className="mr-4" href=""> Result</Link>
+        <Link className="mr-4" href=""> Courses</Link>
+        <img src="https://www.governorsindh.com/_next/static/media/logo.9ff76f62.png" alt="logo" width="90" height="113" className="ml-5 -mt-6"/>
+        <img src="https://www.governorsindh.com/bg_house.jpg" alt="banner" width="1500" height="700" className="mt-9 h-96"/>
+      </div>
+      <div>
+        <img src="https://www.governorsindh.com/_next/static/media/cover.1d863e39.png" alt="pic" width="660" height="100" className="ml-auto p-12 -mr-56" />
+      </div>
+      <div className="-mt-72 p-10">
+        <h1 className="text-blue-900 text-left text-5xl  font-extrabold  font-sans -mt-56">Governor Sindh</h1>  
+        <p className="text-blue-900 text-left text-3xl    font-sans mt-6"> Kamran Khan Tessori</p>    
+        <h2 className="text-blue-500 text-left text-4xl  font-bold  font-sans mt-2">Certified Cloud <br /> Applied Generative AI <br /> Engineer (GenEng) </h2>
+        <h3 className="text-blue-900 text-left text-2xl  font-bold  font-sans mt-2">Earn up to $5,000 / month</h3>
+        <h3 className="text-blue-900 text-left text-2xl  font-bold  font-sans mt-2">Now admissions are open in <br /> Hyderabad</h3>
+        <button className="bg-blue-900 mt-4 w-40 p-4 text-white font-bold border-0 rounded-lg cursor-pointer">Apply Now</button>
+        <h1 className="text-blue-900 text-left text-2xl  font-bold font-sans ml-64 -mt-14">562,143 </h1><br />
+        <p className="text-blue-900 text-left text-2xl  font-bold  font-sans ml-48 -mt-8">Accepted Applications</p>
+      </div>
+      <div>
+        <br />
+        <h2 className="text-blue-900  text-3xl  font-extrabold  font-sans text-center">Certified Cloud Applied Generative AI Engineer (GenEng) and Solopreneur</h2>
+        <h2 className="text-blue-900  text-3xl  font-extrabold  font-sans text-center">Developing Billion-Dollar Valued Developers and Solopreneurs</h2>
+        <p className="mt-4 text-center text-black font-semibold font-sans text-lg">The pace of technological change is accelerating, big players like Microsoft, Amazon, Google, and OpenAI are winning by providing infrastructure, large AI foundation models, frameworks, 3D Metaverse experiences, and massive distribution networks. Solopreneurs trained in this program will win by automating work typically outsourced to employees, by directly connecting to customers by eliminating the middleman, and by developing vertical metaverses, thus paving the way for the first billion-dollar valued solopreneur businesses. This program has the objective to train this new breed of billion-dollar solopreneurs. These solopreneurs will adopt the ultra-lean business model and work independently and will not need to hire employees or other team members.</p>
+      </div>
+      <br />
+      <div >
+        <table className="m-auto">
+          <tr>
+            <td><img src="https://www.governorsindh.com/_next/static/media/imageWebsite.5c6ae62f.jpg" alt="" width="430" className="rounded-lg"/></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/imageWebsite3.b845fe78.jpg" alt="" width="430" className="rounded-lg"/></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/imageWebsite2.a102c7b5.jpg" alt="" width="470" className="rounded-lg"/></td>
+          </tr>
+        </table>
+      </div>
+      <div className="rounded-full">
+        <table>
+          <tr className="p-8">
+            <td><img src="https://www.governorsindh.com/_next/static/media/slideShow1.6f890b58.jpg" alt="slider" width="900" className="p-2 rounded-3xl" /></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/slideShow3.0006489a.jpg" alt="slider"  width="1012" className="p-2 rounded-3xl"/></td>
+          </tr>
+        </table>
+      </div>
+      <div>
+          <h2 className="text-blue-900 text-left text-3xl  font-extrabold  font-sans ml-4 mt-4">Core Courses Sequence</h2>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <td><img src="https://www.governorsindh.com/_next/static/media/programming_fundamentals.49cca4e9.jpg" alt="img" width="400" className="ml-2 rounded mt-4" /></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/nextjs.3dff0f70.jpg" alt="img" width="400" className="ml-2 rounded mt-4"/></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/earn_as_your_learn.b8248a49.jpg" alt="img" width="400p"  className="ml-2 rounded mt-4" /></td>
+          </tr>
+          <tr>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-3 rounded-lg ml-2">Programming Fundamentals</p></td>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-2 rounded-lg ml-2">Web 2 Using Next Js</p></td>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-2 rounded-lg ml-2">Earn as You Learn</p></td>
+          </tr>
+        </table>
+      </div>
+      <div>
+          <h2 className="text-blue-900 text-left text-3xl  font-extrabold  font-sans ml-4 mt-4">Advanced Courses</h2>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <td><img src="https://www.governorsindh.com/_next/static/media/AI.39397d24.jpg" alt="img" width="300" className="ml-2 rounded mt-4" /></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/metaverse.06eccb60.jpg" alt="img" width="300" className="ml-2 rounded mt-4"/></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/cloudComputing.7260492c.jpg" alt="img" width="300"  className="ml-2 rounded mt-4" /></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/iot.16f7b003.jpg" alt="img" width="300"  className="ml-2 rounded mt-4" /></td>
+          </tr>
+          <tr>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-2 rounded-lg ml-3">Artificial Intelligence</p></td>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-2 rounded-lg ml-2">Web 3 and Metaverse</p></td>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-2 rounded-lg ml-2">Cloud-Native Computing</p></td>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-2 rounded-lg ml-2">Ambient Computing and IoT</p></td>
+          </tr>
+          <tr>
+            <td><img src="https://www.governorsindh.com/_next/static/media/genomics.b87789f1.jpg" alt="img" width="300" className="ml-2 rounded mt-4" /></td>
+            <td><img src="https://www.governorsindh.com/_next/static/media/automation.a77dbbe8.jpg" alt="img" width="300" className="ml-2 rounded mt-4"/></td>
+          </tr>
+          <tr>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-2 rounded-lg ml-2">Genomics and Bioinformatics</p></td>
+            <td><p className="text-center font-sans font-3xl font-bold border-2 border-gray-300 text-gray-700 p-2 -mt-2 rounded-lg ml-2">Network Programmability and <br /> Automation </p></td>
+          </tr>
+        </table>
+      </div>
+      <div className="bg-gray-200 h-80 mt-12">
+        <div >
+          <br />
+          <h1 className="text-xl font-bold ml-4">Core Courses</h1>
+        <div>
+          <div className="text-lg ml-5 mt-2">Programming Fundamentals</div>
+          <div className="text-lg ml-5 mt-2">Web2 Using NextJS</div>
+          <div className="text-lg ml-5 mt-2">Earn as You Learn</div>
+        </div>
+        </div>
+        <div className="ml-44 -mt-40">
+          <br />
+          <h1 className="text-xl font-bold ml-80">Advanced Courses</h1>
+        <div>
+          <div className="text-lg ml-80 mt-2">Web 3 and Metaverse</div>
+          <div className="text-lg ml-80 mt-2">Cloud-Native Computing</div>
+          <div className="text-lg ml-80 mt-2">Artificial Intelligence (AI) and Deep Learning</div>
+          <div className="text-lg ml-80 mt-2">Ambient Computing and IoT</div>
+          <div className="text-lg ml-80 mt-2">Genomics and Bioinformatics</div>
+          <div className="text-lg ml-80 mt-2">Network Programmability and Automation</div>
+        </div> 
+        </div>
+        <div className="ml-96 -mt-40 px-52">
+          <br />
+          <h1 className="text-xl font-bold ml-80 -mt-28">Social Links</h1>
+        <div className="text-lg ml-80 mt-2 justify-center items-center rounded-full h-10">
+        <img src="https://static.vecteezy.com/system/resources/previews/018/930/476/non_2x/facebook-logo-facebook-icon-transparent-free-png.png" alt="logo" width="50" className="mr-2 h-12"/><Link href=""></Link>
+        <img src="https://static.vecteezy.com/system/resources/previews/023/986/704/non_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png" alt="logo" width="50" className="-mt-12 ml-10 h-12" /><Link href=""></Link>
+          <img src="https://dwglogo.com/wp-content/uploads/2019/02/Twitter_logo.png" alt="logo"  width="50" className="-mt-12  ml-20 h-12"/><Link href=""></Link>
+          <img src="https://static.vecteezy.com/system/resources/previews/018/930/415/non_2x/instagram-logo-instagram-icon-transparent-free-png.png" alt="logo" width="50" className="-mt-12 ml-28 h-12"/><Link href=""></Link>
+          <img src="https://static.vecteezy.com/system/resources/previews/016/716/450/non_2x/tiktok-icon-free-png.png" alt="logo" width="40" className="-mt-12 ml-40 h-"/><Link href=""></Link>
+          <div className="mt-4 ml-12">
+          <Link href="mailto:education@govenersindh.com" className="text-main underline">education@governorsindh.com<img src="https://static.vecteezy.com/system/resources/previews/035/264/512/non_2x/3d-blue-mail-envelope-icon-of-email-message-concept-free-png.png" alt="" width="40" className="-mt-8 -ml-10"/></Link>
+          </div>
+        </div> 
+        </div>
+      </div>
+        
+      </div>
+      );
+    }
